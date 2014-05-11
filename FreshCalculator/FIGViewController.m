726855b19aa459563c1480e9ef0b982ffb6ce7c7
John Figueiredo <john@johnfigueiredo.com>
2014-05-11 16:03:47 -0700

    Added digitPressed, clearButton, percentButton, and negativeOrPositiveButtonPressed methods

--- FreshCalculator/FIGViewController.m
+++ FreshCalculator/FIGViewController.m
@@ -10,7 +10,8 @@
 
 @interface FIGViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *calulatorDisplay;
-
+@property (nonatomic, getter = isStillTypingNumber) BOOL stillTypingNumber;
+@property (nonatomic, getter = isNegativeNumber) BOOL negativeNumber;
 @end
 
 @implementation FIGViewController
@@ -19,6 +20,47 @@
   [super viewDidLoad];
 	[self setCalculatorDisplay];
 }
+
+#pragma mark - IBActions
+- (IBAction)digitPressed:(id)sender {
+  NSString *digit = [[sender titleLabel] text];
+  if ([self.calulatorDisplay.text isEqualToString:@"0"]) {
+    [self.calulatorDisplay setText:digit];
+    self.stillTypingNumber = YES;
+  } else if ([self isStillTypingNumber]) {
+    [self.calulatorDisplay setText:[[self.calulatorDisplay text] stringByAppendingString:digit]];
+  } else {
+    [self.calulatorDisplay setText:digit];
+    self.stillTypingNumber = YES;
+  }
+}
+
+- (IBAction)clearButtonPressed:(id)sender {
+  [self.calulatorDisplay setText:@"0"];
+  self.stillTypingNumber = NO;
+}
+
+- (IBAction)percentButtonPressed:(id)sender {
+  NSString *string = self.calulatorDisplay.text;
+  float number = [string floatValue];
+  float percent = number / 100.0f;
+  NSString *finalString = [[NSNumber numberWithFloat:percent] stringValue];
+  [self.calulatorDisplay setText:finalString];
+}
+
+- (IBAction)negativeOrPositiveButonPressed:(id)sender {
+  NSString *originalString = self.calulatorDisplay.text;
+  
+  if ([self isNegativeNumber]) {
+    NSString *newString = [originalString substringWithRange:NSMakeRange(1, [originalString length]-1)];
+    [self.calulatorDisplay setText:newString];
+    self.negativeNumber = NO;
+  } else if (![originalString isEqualToString:@"0"]) {
+    [self.calulatorDisplay setText:[@"-" stringByAppendingString:originalString]];
+    self.negativeNumber = YES;
+  }
+}
+
 
 #pragma mark - Private Methods
 - (UIStatusBarStyle)preferredStatusBarStyle {
